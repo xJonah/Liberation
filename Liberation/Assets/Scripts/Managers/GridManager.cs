b6fbc4f17ee4b393f638514c23d360ee2a5a46b0
xJonah <93002456+xJonah@users.noreply.github.com>
2021-11-29 21:00:30 +0000

    map updates

--- Liberation/Assets/Scripts/Managers/GridManager.cs
+++ Liberation/Assets/Scripts/Managers/GridManager.cs
@@ -7,18 +7,8 @@ using Random=UnityEngine.Random;
 
 public class GridManager : MonoBehaviour {
     [SerializeField] public int width, height;
-    [SerializeField] private Tile grassTile, desertTile;
-    [SerializeField] private Transform _cam;
-
-    public GameObject orcPrefab;
-
-    /*
-    public static GridManager Instance;
-    //GridManager instance created
-    void Awake() {
-        Instance = this;
-    }
-    */
+    [SerializeField] private Tile grassTile, desertTile, mountainTile, oceanTile;
+    [SerializeField] private Transform cam;
 
     private void Start() {
         GenerateGrid();
@@ -26,28 +16,46 @@ public class GridManager : MonoBehaviour {
  
     public void GenerateGrid() {
 
-        //Grass tile spawn
+        //Grass tile spawn (Top left)
          for (int x = 0; x < width / 2; x++) {
-            for (int y = 0; y < height; y++) {
+            for (int y = 5; y < height; y++) {
 
                 var spawnedGrassTiles = Instantiate(grassTile, new Vector3(x, y), Quaternion.identity);
-                spawnedGrassTiles.name = $"Tile {x} {y}";
+                spawnedGrassTiles.name = $"GrassTile {x} {y}";
                 spawnedGrassTiles.Init(x, y);
              }
         }
 
-        //Desert tile spawn
-        for (int x = 8; x < width ; x++) {
-            for (int y = 0; y < height; y++) {
+        //Desert tile spawn (Top right)
+        for (int x = 8; x < width; x++) {
+            for (int y = 5; y < height; y++) {
 
                 var spawnedDesertTiles = Instantiate(desertTile, new Vector3(x, y), Quaternion.identity);
-                spawnedDesertTiles.name = $"Tile {x} {y}";
+                spawnedDesertTiles.name = $"DesertTile {x} {y}";
                 spawnedDesertTiles.Init(x, y);
              }
         }
 
+        //Mountain tile spawn (Bottom left)
+        for (int x = 0; x < width / 2; x++) {
+            for (int y = 0; y < height / 2; y++) {
+                var spawnedMountainTiles = Instantiate(mountainTile, new Vector3(x, y), Quaternion.identity);
+                spawnedMountainTiles.name = $"MountainTile {x} {y}";
+                spawnedMountainTiles.Init(x, y);
+             }
+        }
+
+        //Ocean tile spawn (Bottom right)
+        for (int x = 8; x < width; x++) {
+            for (int y = 0; y < height / 2; y++) {
+                 var spawnedOceanTiles = Instantiate(oceanTile, new Vector3(x, y), Quaternion.identity);
+                spawnedOceanTiles.name = $"OceanTile {x} {y}";
+                spawnedOceanTiles.Init(x, y);
+             }
+        }
+
         //Camera setup
-        _cam.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 - 0.5f,-10);
+        cam.transform.position = new Vector3((float) width / 2 - 0.5f, (float) height / 2 - 0.5f, -10);
 
     }
 
