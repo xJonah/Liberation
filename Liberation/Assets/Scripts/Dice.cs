f2e94b1b2618221ac74657f27e66894a04fcef81
niyazahmed2001 <niyazahmed01@outlook.com>
2022-03-30 13:38:25 +0100

    Dice implemented

---
+++ Liberation/Assets/Scripts/Dice.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+    // Array of dice sides sprites to load from Resources folder
+    public GameObject[] diceSides;
+    [SerializeField] public Button roll;
+
+    // Reference to sprite renderer to change sprites
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	private void Start () {
+
+        // Assign Renderer component
+        rend = GetComponent<SpriteRenderer>();
+
+        // Load dice sides sprites to array from DiceSides subfolder of Resources folder
+        // diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+
+	}
+	
+    // If you left click over the dice then RollTheDice coroutine is started
+    public void RollDice ()
+    {
+        Debug.Log("Roll dice clicked");
+        StartCoroutine("RollTheDice");
+    }
+
+    // Coroutine that rolls the dice
+    private IEnumerator RollTheDice()
+    {
+        // Variable to contain random dice side number.
+        // It needs to be assigned. Let it be 0 initially
+        int randomDiceSide = 0;
+
+        // Final side or value that dice reads in the end of coroutine
+        int finalSide = 0;
+
+        // Loop to switch dice sides ramdomly
+        // before final side appears. 20 itterations here.
+        for (int i = 0; i <= 20; i++)
+        {
+            Debug.Log("Rolled");
+            // Pick up random value from 0 to 5 (All inclusive)
+            randomDiceSide = Random.Range(0, 5);
+            Debug.Log("Dice number is:" + randomDiceSide);
+
+            // Set sprite to upper face of dice from array according to random value
+            for (int x = 0; x < 6; x++){
+                diceSides[x].SetActive(false);
+            }
+
+            diceSides[randomDiceSide].SetActive(true);
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        // Assigning final side so you can use this value later in your game
+        // for player movement for example
+        finalSide = randomDiceSide + 1;
+
+        // Show final dice value in Console
+        Debug.Log(finalSide);
+    }
+}
--- Liberation/Assets/Scripts/Tiles/Tile.cs
+++ Liberation/Assets/Scripts/Tiles/Tile.cs
@@ -5,8 +5,9 @@ using Photon.Pun;
 using UnityEngine.UI;
 using TMPro;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
- 
-public abstract class Tile : MonoBehaviour {
+
+public abstract class Tile : MonoBehaviour
+{
 
     [SerializeField] protected SpriteRenderer _renderer;
 
@@ -18,12 +19,14 @@ public abstract class Tile : MonoBehaviour {
 
 
     // Allow override in order for Tiles to have checkerboard pattern or not
-    public virtual void Init(int x, int y) {
-        
+    public virtual void Init(int x, int y)
+    {
+
     }
- 
+
     // Highlight Function when mouse hover over tile
-    void OnMouseEnter() {
+    void OnMouseEnter()
+    {
         _highlight.SetActive(true);
         MenuManager.Instance.ShowTileInfo(this);
     }
@@ -37,10 +40,13 @@ public abstract class Tile : MonoBehaviour {
 
 
     // Grid square is occupied when a unit is on it
-    public void SetUnit(BaseUnit unit) { //Task - Assign counter to each unit/tile as well
+    public void SetUnit(BaseUnit unit)
+    { //Task - Assign counter to each unit/tile as well
 
-        if (PhotonNetwork.LocalPlayer.IsMasterClient) {
-            if (unit.OccupiedTile != null) {
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            if (unit.OccupiedTile != null)
+            {
                 unit.OccupiedTile.OccupiedUnit = null;
             }
             //Only master client moves the initial position of spawned units
@@ -48,7 +54,8 @@ public abstract class Tile : MonoBehaviour {
             OccupiedUnit = unit;
             unit.OccupiedTile = this;
         }
-        else {
+        else
+        {
             OccupiedUnit = unit;
             unit.OccupiedTile = this;
         }
@@ -66,7 +73,8 @@ public abstract class Tile : MonoBehaviour {
     */
 
     //On mouse down surrouding tiles are highlighted
-    void OnMouseDown() {
+    void OnMouseDown()
+    {
         tiles = new Dictionary<Vector2, Tile>();
         tiles = GridManager.Instance.GetTiles();
         ArrayList tileVectors = GridManager.Instance.GetSurroundingTiles(this);
@@ -76,7 +84,7 @@ public abstract class Tile : MonoBehaviour {
             //Game has ended
             return;
         }
-        
+
         else if (PhotonNetwork.IsConnected && !GameManager.Instance.IsMyTurn())
         {
             //Not my turn
@@ -84,33 +92,44 @@ public abstract class Tile : MonoBehaviour {
         }
 
         //HUMAN TURN
-        if (GameManager.Instance.GameState == GameState.HumanTurn) {
-            if (OccupiedUnit != null) {
-                if (OccupiedUnit.Faction == Faction.Human) {
+        if (GameManager.Instance.GameState == GameState.HumanTurn)
+        {
+            if (OccupiedUnit != null)
+            {
+                if (OccupiedUnit.Faction == Faction.Human)
+                {
                     UnitManager.Instance.SetSelectedHuman((BaseHuman)OccupiedUnit);
-                    foreach (Vector2 v in tileVectors) {
+                    foreach (Vector2 v in tileVectors)
+                    {
                         var tile = tiles[v];
-                        if (tile.OccupiedUnit.Faction != Faction.Human) {
+                        if (tile.OccupiedUnit.Faction != Faction.Human)
+                        {
                             tile._highlight.SetActive(true);
                         }
                     }
-                } 
-                else {
-                    if(UnitManager.Instance.SelectedHuman != null) {
-                        foreach (Vector2 v in tileVectors) {
+                }
+                else
+                {
+                    if (UnitManager.Instance.SelectedHuman != null)
+                    {
+                        foreach (Vector2 v in tileVectors)
+                        {
                             var tile = tiles[v];
                             tile._highlight.SetActive(false);
                         }
 
                         var enemy = OccupiedUnit;
                         var result1 = DiceBattle();
-                        if (result1) {
+                        if (result1)
+                        {
                             PhotonNetwork.Destroy(enemy.gameObject);
                             UnitManager.Instance.SpawnNewHuman(enemy.OccupiedTile);
                             //MenuManager.Instance.ShowBattleWin();
                             UnitManager.Instance.SetSelectedHuman(null);
                             GameManager.Instance.ChangeState(GameState.OrcTurn);
-                        } else if (!result1) {
+                        }
+                        else if (!result1)
+                        {
                             //MenuManager.Instance.ShowBattleLoss();
                             UnitManager.Instance.SetSelectedHuman(null);
                             GameManager.Instance.ChangeState(GameState.OrcTurn);
@@ -121,22 +140,30 @@ public abstract class Tile : MonoBehaviour {
         }
 
         //ORC TURN
-        else if (GameManager.Instance.GameState == GameState.OrcTurn) {
-            if (OccupiedUnit != null) {
-                if (OccupiedUnit.Faction == Faction.Orc) {
+        else if (GameManager.Instance.GameState == GameState.OrcTurn)
+        {
+            if (OccupiedUnit != null)
+            {
+                if (OccupiedUnit.Faction == Faction.Orc)
+                {
                     UnitManager.Instance.SetSelectedOrc((BaseOrc)OccupiedUnit);
-                } 
-                else {
-                    if(UnitManager.Instance.SelectedOrc != null) {
+                }
+                else
+                {
+                    if (UnitManager.Instance.SelectedOrc != null)
+                    {
                         var enemy = OccupiedUnit;
                         var result1 = DiceBattle();
-                        if (result1) {
-                            PhotonNetwork.Destroy(enemy.gameObject);   
+                        if (result1)
+                        {
+                            PhotonNetwork.Destroy(enemy.gameObject);
                             //MenuManager.Instance.ShowBattleWin();                  
                             UnitManager.Instance.SetSelectedOrc(null);
                             //GameManager.Instance.ChangeState(GameState.ElfTurn); 
                             GameManager.Instance.ChangeState(GameState.HumanTurn); //For Development Process
-                        } else if (!result1) {
+                        }
+                        else if (!result1)
+                        {
                             //MenuManager.Instance.ShowBattleLoss();          
                             UnitManager.Instance.SetSelectedOrc(null);
                             //GameManager.Instance.ChangeState(GameState.ElfTurn);
@@ -146,24 +173,32 @@ public abstract class Tile : MonoBehaviour {
                 }
             }
         }
-        
+
         //ELF TURN
-        else if (GameManager.Instance.GameState == GameState.ElfTurn) {
-            if (OccupiedUnit != null) {
-                if (OccupiedUnit.Faction == Faction.Elf) {
+        else if (GameManager.Instance.GameState == GameState.ElfTurn)
+        {
+            if (OccupiedUnit != null)
+            {
+                if (OccupiedUnit.Faction == Faction.Elf)
+                {
                     UnitManager.Instance.SetSelectedElf((BaseElf)OccupiedUnit);
-                } 
-                else {
-                    if(UnitManager.Instance.SelectedElf != null) {
+                }
+                else
+                {
+                    if (UnitManager.Instance.SelectedElf != null)
+                    {
                         var enemy = OccupiedUnit;
                         var result1 = DiceBattle();
-                        if (result1) {
-                            PhotonNetwork.Destroy(enemy.gameObject);   
+                        if (result1)
+                        {
+                            PhotonNetwork.Destroy(enemy.gameObject);
                             //MenuManager.Instance.ShowBattleWin();                  
                             UnitManager.Instance.SetSelectedElf(null);
                             GameManager.Instance.ChangeState(GameState.DwarfTurn);
-                        } else if (!result1) {
-                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedElf.gameObject); 
+                        }
+                        else if (!result1)
+                        {
+                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedElf.gameObject);
                             //MenuManager.Instance.ShowBattleLoss();          
                             UnitManager.Instance.SetSelectedElf(null);
                             GameManager.Instance.ChangeState(GameState.DwarfTurn);
@@ -174,23 +209,31 @@ public abstract class Tile : MonoBehaviour {
         }
 
         //DWARF TURN
-        else if (GameManager.Instance.GameState == GameState.DwarfTurn) {
-            if (OccupiedUnit != null) {
-                if (OccupiedUnit.Faction == Faction.Dwarf) {
+        else if (GameManager.Instance.GameState == GameState.DwarfTurn)
+        {
+            if (OccupiedUnit != null)
+            {
+                if (OccupiedUnit.Faction == Faction.Dwarf)
+                {
                     UnitManager.Instance.SetSelectedDwarf((BaseDwarf)OccupiedUnit);
-                } 
-                else {
-                    if(UnitManager.Instance.SelectedDwarf != null) {
+                }
+                else
+                {
+                    if (UnitManager.Instance.SelectedDwarf != null)
+                    {
                         var enemy = OccupiedUnit;
                         var result1 = DiceBattle();
-                        if (result1) {
-                            PhotonNetwork.Destroy(enemy.gameObject);   
-                           // MenuManager.Instance.ShowBattleWin();                  
+                        if (result1)
+                        {
+                            PhotonNetwork.Destroy(enemy.gameObject);
+                            // MenuManager.Instance.ShowBattleWin();                  
                             UnitManager.Instance.SetSelectedDwarf(null);
                             GameManager.Instance.ChangeState(GameState.DemonTurn);
-                        } else if (!result1) {
-                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedDwarf.gameObject); 
-                           // MenuManager.Instance.ShowBattleLoss();          
+                        }
+                        else if (!result1)
+                        {
+                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedDwarf.gameObject);
+                            // MenuManager.Instance.ShowBattleLoss();          
                             UnitManager.Instance.SetSelectedDwarf(null);
                             GameManager.Instance.ChangeState(GameState.DemonTurn);
                         }
@@ -201,23 +244,31 @@ public abstract class Tile : MonoBehaviour {
         }
 
         //DEMON TURN
-        else if (GameManager.Instance.GameState == GameState.DemonTurn) {
-            if (OccupiedUnit != null) {
-                if (OccupiedUnit.Faction == Faction.Demon) {
+        else if (GameManager.Instance.GameState == GameState.DemonTurn)
+        {
+            if (OccupiedUnit != null)
+            {
+                if (OccupiedUnit.Faction == Faction.Demon)
+                {
                     UnitManager.Instance.SetSelectedDemon((BaseDemon)OccupiedUnit);
-                } 
-                else {
-                    if(UnitManager.Instance.SelectedDemon != null) {
+                }
+                else
+                {
+                    if (UnitManager.Instance.SelectedDemon != null)
+                    {
                         var enemy = OccupiedUnit;
                         var result1 = DiceBattle();
-                        if (result1) {
-                            PhotonNetwork.Destroy(enemy.gameObject);   
-                          //  MenuManager.Instance.ShowBattleWin();                  
+                        if (result1)
+                        {
+                            PhotonNetwork.Destroy(enemy.gameObject);
+                            //  MenuManager.Instance.ShowBattleWin();                  
                             UnitManager.Instance.SetSelectedDemon(null);
                             GameManager.Instance.ChangeState(GameState.HumanTurn);
-                        } else if (!result1) {
-                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedDemon.gameObject); 
-                           // MenuManager.Instance.ShowBattleLoss();          
+                        }
+                        else if (!result1)
+                        {
+                            PhotonNetwork.Destroy(UnitManager.Instance.SelectedDemon.gameObject);
+                            // MenuManager.Instance.ShowBattleLoss();          
                             UnitManager.Instance.SetSelectedDemon(null);
                             GameManager.Instance.ChangeState(GameState.HumanTurn);
                         }
@@ -228,7 +279,8 @@ public abstract class Tile : MonoBehaviour {
     }
 
     //Dice 2vs2 Battles
-    public bool DiceBattle() {
+    public bool DiceBattle()
+    {
 
         bool result = false;
 
@@ -236,10 +288,12 @@ public abstract class Tile : MonoBehaviour {
 
         int enemyDiceResult = Random.Range(1, 6);
 
-        if (playerDiceResult > enemyDiceResult) {
+        if (playerDiceResult > enemyDiceResult)
+        {
             result = true;
-        } 
-        else {
+        }
+        else
+        {
             result = false;
         }
 
@@ -247,4 +301,3 @@ public abstract class Tile : MonoBehaviour {
     }
 
 }
-
